02443dd35290123656fa3c006d288e26c6399e82
Jahanzaib-1602 <pyscho665@gmail.com>
2024-02-10 04:45:01 +0500

    Initial Commit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
- 
- const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
- 
+Shery.mouseFollower({
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+    debug: true,
+  }); 
+  Shery.makeMagnet(".fas" , {
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
