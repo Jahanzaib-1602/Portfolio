fdb7517eaa76b3744a13ed84afb441fc7bb48460
Jahanzaib-1602 <pyscho665@gmail.com>
2024-06-12 16:39:59 +0500

    animation timer

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ var loader = document.getElementById('preloader');
 window.addEventListener('load', function() {
   setTimeout(function() {
     loader.style.display = 'none';
-  }); 
+  },2000); 
 });
 
 
